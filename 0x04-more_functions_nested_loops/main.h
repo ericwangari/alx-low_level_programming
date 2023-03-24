28a555ba3f04f871c50887871c86656a0bbaa034
ericwangari <njugunaeric82@gmail.com>
2023-03-24 14:52:46 -0700

    printnumbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,4 +10,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
