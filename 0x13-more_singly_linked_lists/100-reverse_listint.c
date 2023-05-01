7cc3fd97c56a8a14908fba7ecd22ff4648ec3d54
ericwangari <njugunaeric82@gmail.com>
2023-05-01 06:32:54 -0700

    print list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,7 +5,7 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of the uique nodes
+ * looped_listint_len - Counts the number of the unique nodes
  * in a looped listint_t linked list.
  * @head: A pointer ti the head of the listint_t to check.
  *
