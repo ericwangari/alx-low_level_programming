f912881ef41426eb95e893ca41dbf0a52e7abc77
ericwangari <njugunaeric82@gmail.com>
2023-05-08 11:46:02 -0700

    task 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_context: A pointer to string to write to the file
  *
  * Return: If the function fails - -1
- * Otherwise - 1.
+ *   Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -25,6 +25,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1 || w == -1)
 		return (-1);
+
 	close(fd);
+
 	return (1);
 }
