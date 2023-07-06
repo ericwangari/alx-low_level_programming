5f0768fb1361de9849f34a01ed382f9660e7a606
ericwangari <njugunaeric82@gmail.com>
2023-07-06 10:02:03 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,27 @@
 #include "main.h"
-/**
- * binary_to_unit - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
- */
-unsigned int binary_to_unit(const char *b)
-{
-	int i;
-	unsigned int dec_val = 0;
 
-	if (!b)
-		return (0);
-	for (i = 0; b[i]; i++)
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
 	}
-	return (dec_val);
+	if (!count)
+		_putchar('0');
 }
+
